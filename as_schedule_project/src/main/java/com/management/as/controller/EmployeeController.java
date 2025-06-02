0e1926a6c03897dec8742de5567c43f7e7031efc
GRCOM-25 <GRCOM-25@DESKTOP-J64AP5A>
2025-06-02 17:44:57 +0900

    uy8ik

--- as_schedule_project/src/main/java/com/management/as/controller/EmployeeController.java
+++ as_schedule_project/src/main/java/com/management/as/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.management.as.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.management.as.domain.Criterai;
 import com.management.as.domain.EmployeeVO;
+import com.management.as.domain.PageDTO;
+import com.management.as.service.BoardService;
 import com.management.as.service.EmployeeService;
 
 
@@ -24,7 +28,11 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class EmployeeController {
 
+	@Autowired
 	private EmployeeService service;
+	
+	@Autowired
+	private BoardService boardService;
 
 	@GetMapping("/schedule")
 	public void schedule(Model model) {
@@ -55,7 +63,23 @@ public class EmployeeController {
         }
 	}
 	
-
+	// 공지사항
+	@GetMapping("/board/list")
+	public String list(Criterai cri, Model model) {
+		cri.setBoardType("E");
+		model.addAttribute("list", boardService.getListWithPaging(cri));
+		int total = boardService.getTotalCount(cri);
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
+		model.addAttribute("keyword", cri.getKeyword());
+		return "/employee/board/list";
+	}
+	
+	// 목록 리스트에서 제목을 클릭하여 상세내용 화면으로 이동
+	@GetMapping("/board/get")
+	public String get(@RequestParam("bno") int bno, Model model) {
+		model.addAttribute("board", boardService.get(bno));
+		return "/employee/board/view";
+	}
 	
 
 }
