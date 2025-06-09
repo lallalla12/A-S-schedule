ad947f4474a7fd770e70bbb01a2fcf0cd81380b2
rynhaa <rynhaa@naver.com>
2025-06-09 12:33:07 +0900

    관리자 AS현황 배정 팝업 소스 복구, 직원목록 팝업 수정중

--- as_schedule_project/src/main/java/com/management/as/service/EmployeeService.java
+++ as_schedule_project/src/main/java/com/management/as/service/EmployeeService.java
@@ -40,6 +40,8 @@ public interface EmployeeService {
 
 	public List<CustomerVO> searchByEmployeeAndField(String eno, String searchField, String keyword);
 
+	public List<ScheduleVO> getASSchedule();
+
 
 	 
 }
